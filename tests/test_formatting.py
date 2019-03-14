ba69ed3a8cfa8253c70fcb6d470d64469a53c836
Chilipp <p.sommer@posteo.de>
2019-03-14 21:54:50 +0100

    Minor bug fix

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -12,15 +12,15 @@ def test_newline_chars(meta_row):
             meta_row).all()
 
 
-@pytest.mark.parametrize('doi', ['DOI1', 'DOI2', 'DOI3', 'DOI4', 'DOI5'])
-def test_doi(meta_row, doi):
-    if meta_row.notnull()[doi]:
-        doi = meta_row.copy(True)[doi]
+@pytest.mark.parametrize('doi_field', ['DOI1', 'DOI2', 'DOI3', 'DOI4', 'DOI5'])
+def test_doi(meta_row, doi_field):
+    if meta_row.notnull()[doi_field]:
+        doi = meta_row.copy(True)[doi_field]
 
         for patt in ['https://doi.org/', 'http://dx.doi.org/', 'doi: ',
                      'doi.org/', 'DOI: ', 'DOI ', 'doi:', ' ',
                      'https://link.springer.com/article/']:
             doi = doi.replace(patt, '')
-        assert doi == meta_row[doi]
+        assert doi == meta_row.loc[doi_field]
     else:
-        pytest.skip("%s field empty specified" % doi)
+        pytest.skip("%s field empty specified" % doi_field)
