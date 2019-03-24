031772a8399a7cf780e31e8dd8af0eaf71b48bf0
Chilipp <p.sommer@posteo.de>
2019-03-24 22:31:02 +0100

    added a script to update fixed tables

---
+++ postgres/scripts/updateFixedTables.py
@@ -0,0 +1,65 @@
+import sys
+import os.path as osp
+import psycopg2 as psql
+import pandas as pd
+import numpy as np
+from functools import partial
+
+db_url = sys.argv[1]
+to_update = sys.argv[2:]
+
+conn = psql.connect(db_url)
+cursor = conn.cursor()
+
+
+def is_null_str(x):
+    return 'NULL' if x == "" else "'" + x.strip().replace("'", " ") + "'"
+
+
+tables = osp.join(osp.dirname(__file__), "tables", '')
+
+read_tsv = partial(pd.read_csv, sep='\t')
+
+# ---
+if 'Country' in to_update or 'all' in to_update:
+    countries = read_tsv(tables + "Country.tsv")
+    countries.replace(np.nan, '', inplace=True)
+
+    for x in range(countries.shape[0]):
+        cursor.execute(
+            "UPDATE countries SET natural_earth = %s WHERE country = '%s'" % (
+                is_null_str(countries.iloc[x][1]), countries.iloc[x][0]))
+
+    conn.commit()
+
+
+# ---
+if 'SampleType' in to_update or 'all' in to_update:
+    sampleType = read_tsv(tables + "SampleType.tsv")
+    sampleType.replace(np.nan, '', inplace=True)
+
+    for x in range(sampleType.shape[0]):
+        cursor.execute(
+            "UPDATE sampleTypes SET notes = %s WHERE sampleType = '%s'" % (
+                is_null_str(sampleType.iloc[x][1]), sampleType.iloc[x][0]))
+
+    conn.commit()
+
+
+# ---
+if 'GroupID' in to_update or 'all' in to_update:
+    groupIDs = read_tsv(tables + "GroupID.tsv")
+    groupIDs.replace(np.nan, '', inplace=True)
+
+    for x in range(groupIDs.shape[0]):
+        cursor.execute(
+            "UPDATE groupID SET groupname = %s, higher_groupid = %s "
+            "WHERE groupID = %s" % (
+                is_null_str(groupIDs.iloc[x][1]),
+                is_null_str(groupIDs.iloc[x][2]),
+                is_null_str(groupIDs.iloc[x][0])))
+
+    conn.commit()
+
+
+conn.close()
